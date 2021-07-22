f9849086f57403fc956f3b8a64ed4f99c800aad7
Derrick Bradbury <derrickb@halex.com>
2021-07-22 15:47:37 -0400

    Update on deploy

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -4,6 +4,6 @@ var beaconApp = artifacts.require("combine_beacon");
 module.exports = function(deployer, network, accounts) {
     deployer.deploy(beaconApp);
     deployer.deploy(combineApp);
-    deployer.deploy(proxyApp, combineApp.address, accounts[1]);
+    deployer.deploy(proxyApp, combineApp.address, beaconApp.address, accounts[1]);
     // proxyInst.initialize(411, 10000000000000000000, '0x2320738301305c892B01f44E4E9854a2D19AE19e', '0x2320738301305c892B01f44E4E9854a2D19AE19e');
 };
