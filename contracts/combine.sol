4abbffef381cea837898663f023da3ba0de16bde
Derrick Bradbury <derrickb@halex.com>
2021-07-29 17:09:35 -0400

    move interfaces out of main file

--- contracts/combine.sol
+++ contracts/combine.sol
@@ -1,45 +1,9 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
+import "./Interfaces.sol";
 import "./Storage.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-interface iMasterChef{
-     function pendingCake(uint256 _pid, address _user) external view returns (uint256);
-     function poolInfo(uint _poolId) external view returns (address, uint,uint,uint);
-     function userInfo(uint _poolId, address _user) external view returns (uint,uint);
-     function deposit(uint poolId, uint amount) external;
-     function withdraw(uint poolId, uint amount) external;
-     function cakePerBlock() external view returns (uint);
-     function updatePool(uint poolId) external;
-}
-
-interface iRouter { 
-    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
-    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);
-    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);    
-    function swapExactTokensForTokens(uint amountIn,uint amountOutMin,address[] calldata path,address to,uint deadline) external returns (uint[] memory amounts);
-    function addLiquidityETH(address token,uint amountTokenDesired ,uint amountTokenMin,uint amountETHMin,address to,uint deadline) external payable returns (uint amountToken, uint amountETH, uint liquidity);
-    function addLiquidity(address tokenA,address tokenB,uint amountADesired,uint amountBDesired,uint amountAMin,uint amountBMin,address to,uint deadline) external returns (uint amountA, uint amountB, uint liquidity);
-    function removeLiquidityETH(address token,uint liquidity,uint amountTokenMin,uint amountETHMin,address to,uint deadline) external returns (uint amountToken, uint amountETH);
-    function removeLiquidity(address tokenA,address tokenB, uint liquidity,uint amountAMin,uint amountBMin,address to,uint deadline) external returns (uint amountToken, uint amountETH);
-}
-
-interface iLPToken{
-    function token0() external view returns (address);
-    function token1() external view returns (address);
-    function allowance(address owner, address spender) external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
-    function approve(address spender, uint value) external returns (bool);    
-}
-
-interface iBeacon {
-    function getFee(string memory _exchange, string memory _type, address _user) external returns(uint64);
-}
-
-interface iWBNB {
-    function withdraw(uint wad) external;
-}
 
 contract combineApp is Storage, Ownable, AccessControl {
     event uintLog( string message, uint value);
