1e0a15c8d5a10255561d42b36f276c2456c6dd2f
Derrick Bradbury <derrickb@halex.com>
2022-03-14 16:33:41 -0400

    add docs to slots.sol

--- contracts/utils/slots.sol
+++ contracts/utils/slots.sol
@@ -34,6 +34,13 @@ library slotsLib {
     error SlotOutOfBounds();
     event SlotsUpdated();
 
+
+    ///@notice Add a new exchange/pool to slot pool
+    ///@param _poolId The pool ID
+    ///@param _exchangeName Exchange name
+    ///@param _slots current pool of slots
+    ///@param beaconContract Address of the beacon contract
+    ///@return new position in slot pool    
     function addSlot(uint64 _poolId, string memory _exchangeName, slotStorage[] storage slots,address beaconContract) internal returns (uint64) {
         uint64 _slotId = find_slot(_poolId, _exchangeName, slots);
         if (_slotId != MAX_SLOTS+1) return _slotId;
@@ -43,12 +50,28 @@ library slotsLib {
         return uint64(slots.length - 1);
     }
 
+    ///@notice switch slots between two pools
+    ///@param _fromPoolId The from pool ID
+    ///@param _fromExchangeName The from exchange name
+    ///@param _toPoolId The to pool ID
+    ///@param _toExchangeName The to exchange name
+    ///@param slots current pool of slots
+    ///@param beaconContract Address of the beacon contract
+    ///@return Current slots Pool
     function swapSlot(uint _fromPoolId, string memory _fromExchangeName, uint _toPoolId, string memory _toExchangeName, slotStorage[] storage slots, address beaconContract) internal returns (sSlots memory) {
         uint64 _fromSlotId = find_slot(_fromPoolId, _fromExchangeName, slots);
         if (_fromSlotId == MAX_SLOTS) revert InactivePool(_fromPoolId);
         return updateSlot(_fromSlotId, _toPoolId, _toExchangeName, slots, beaconContract);
     }
 
+
+    ///@notice update slotid with new pool and exchange
+    ///@param _slotId The slot ID
+    ///@param _poolId The pool ID
+    ///@param _exchangeName The exchange name
+    ///@param slots current pool of slots
+    ///@param beaconContract Address of the beacon contract
+    ///@return Current slots Pool
     function updateSlot(uint64 _slotId, uint _poolId, string memory _exchangeName, slotStorage[] storage slots, address beaconContract) internal returns (sSlots memory) {
         iBeacon.sExchangeInfo memory exchangeInfo = iBeacon(beaconContract).getExchangeInfo(_exchangeName);
         (address _lpContract,uint _alloc,,) = iMasterChef(exchangeInfo.chefContract).poolInfo(_poolId);
@@ -80,6 +103,11 @@ library slotsLib {
         return sSlots(uint64(slots[_slotId].poolId),slots[_slotId].exchangeName,slots[_slotId].lpContract, slots[_slotId].token0,slots[_slotId].token1,exchangeInfo.chefContract,exchangeInfo.routerContract,exchangeInfo.rewardToken,exchangeInfo.pendingCall,exchangeInfo.intermediateToken);
     }
 
+    ///@notice Remove slot from pool
+    ///@param _poolId The pool ID
+    ///@param _exchangeName The exchange name
+    ///@param slots current pool of slots
+    ///@return New length of the slots pool
     function removeSlot(uint _poolId, string memory _exchangeName, slotStorage[] storage slots) internal returns (uint) {
         uint _slotId = find_slot(_poolId,_exchangeName,slots);
         if (_slotId >= slots.length) revert SlotOutOfBounds();
@@ -90,6 +118,11 @@ library slotsLib {
         return slots.length;
     }
 
+    ///@notice locate slotid using name and exchange
+    ///@param _poolId The pool ID
+    ///@param _exchangeName The exchange name
+    ///@param slots current pool of slots
+    ///@return slotid from slot pool
     function find_slot(uint _poolId, string memory _exchangeName, slotStorage[] storage slots) private view returns (uint64){
         for(uint64 i = 0;i<slots.length;i++) {
             if (slots[i].poolId == _poolId && keccak256(bytes(slots[i].exchangeName)) == keccak256(bytes(_exchangeName))) { //this is to get around storage type differences...
@@ -98,6 +131,12 @@ library slotsLib {
         }
         return MAX_SLOTS+1;
     }
+
+    ///@notice return slot information baesd on poolid and exchange
+    ///@param _poolId The pool ID
+    ///@param _exchangeName The exchange name
+    ///@param slots current pool of slots
+    ///@return slot sturcture
     function getSlot(uint _poolId, string memory _exchangeName, slotStorage[] storage slots, address beaconContract) internal view returns (sSlots memory) {
         uint64 _slotId = find_slot(_poolId,_exchangeName,slots);
         if (_slotId == MAX_SLOTS+1) return (sSlots(_slotId,"",address(0),address(0),address(0),address(0),address(0),address(0),"",address(0)));
@@ -106,6 +145,12 @@ library slotsLib {
         return sSlots(uint64(slots[_slotId].poolId),slots[_slotId].exchangeName,slots[_slotId].lpContract, slots[_slotId].token0,slots[_slotId].token1,exchangeInfo.chefContract,exchangeInfo.routerContract,exchangeInfo.rewardToken,exchangeInfo.pendingCall,exchangeInfo.intermediateToken);
     }    
 
+    ///@notice when depositing, check if new slot needs to be created before updating
+    ///@param _poolId The pool ID
+    ///@param _exchangeName The exchange name
+    ///@param slots current pool of slots
+    ///@param beaconContract Address of the beacon contract
+    ///@return slot structure
     function getDepositSlot(uint64 _poolId, string memory _exchangeName, slotStorage[] storage slots, address beaconContract) internal returns (sSlots memory) {
         uint64 _slotId = find_slot(_poolId,_exchangeName,slots);
         if (_slotId == MAX_SLOTS+1) {
