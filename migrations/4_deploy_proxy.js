f242aa34d80fab1e28c02e164077f25142eedd59
Derrick Bradbury <derrickb@halex.com>
2022-07-06 12:55:13 -0400

    Add verify plugin

--- migrations/4_deploy_proxy.js
+++ migrations/4_deploy_proxy.js
@@ -4,6 +4,8 @@ var proxyFactory = artifacts.require("proxyFactory");
 module.exports = async function(deployer, network, accounts) {
 
     var beacon = await beaconApp.deployed();
+    // let beacon = {address:"0x8422d0922d3bde86a8A96461Bcd3c301b8588860"}; 
+
     console.log("Beacon: ", beacon.address);
     await deployer.deploy(proxyFactory,beacon.address,accounts[0]);
 
--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,12 @@ console.log(deploy_config.privateKey);
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-  /**
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    bscscan: deploy_config.BSCSCANAPIKEY
+  },  /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
    * will spin up a development blockchain for you on port 9545 when you
